5fb56dbd530426b03ca2647ae680453ba2eb4f33
Nugrahasaputr <161214808+Nugrahasaputr@users.noreply.github.com>
2024-03-14 10:49:33 +0800

    Add files via upload

---
+++ Tugas1_SourceCode_Praktek/Tugas1_13020220098_Nugraha saputra/src/incr.java
@@ -0,0 +1,13 @@
+/*
+Nim  : 13020220098
+Nama : Nugraha Saputra
+--Rabu, 21/02/2024--
+ */
+public class incr {
+    public static void main(String[] args) {
+        int i, j;
+        i = 3;
+        j = i++;
+        System.out.println ("Nilai i : " + (++i) + "\nNilai j : " + j);
+    }
+}
